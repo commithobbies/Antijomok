6245ac9f9d1a0b31bb3e0a9855b33b41383079be
KenXimp <aibwong3@gmail.com>
2024-08-04 22:56:05 +0700

    Femboy

--- modules/start.py
+++ modules/start.py
@@ -1,10 +1,8 @@
-# Copyright 2024 Qewertyy, MIT License
-
 from pyrogram import Client, filters, types as t
 from bot import StartTime
 
 startText = """
-An AntiNSFW bot Powered by @LexicaAPI to protect your groups from NSFW content.
+Hallo saya bot antijomok yang akan menghapus konten nsfw.
 """
 
 @Client.on_message(filters.command(["start","help","repo","source"]))
@@ -14,7 +12,7 @@ async def start(_: Client, m: t.Message):
         reply_markup=t.InlineKeyboardMarkup(
             [
                 [
-                    t.InlineKeyboardButton(text="Source",url="https://github.com/Qewertyy/AntiNSFWRobot")
+                    t.InlineKeyboardButton(text="Source",url="https://github.com/commithobbies/Antijomok")
                 ]
             ]
         )
